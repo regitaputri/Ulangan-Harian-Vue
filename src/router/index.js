8c14ba6aa98bcff06d15fa4d0c71ff327e6d5e64
aldimakarim <mohammad_makarim_25rpl@student.smktelkom-mlg.sch.id>
2020-01-24 15:17:38 +0700

    alhamdulillah selesai T.T

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Data_Kantin from '../views/Data_Kantin.vue'
+import Data_Makanan from '../views/Data_Makanan.vue'
+import Pembelian from '../views/Pembelian.vue'
+import Navbar from '../views/layouts/Navbar.vue'
+import Footer from '../views/layouts/Footer.vue'
+import Sidebar from '../views/layouts/Sidebar.vue'
+
 
 Vue.use(VueRouter)
 
@@ -8,20 +15,30 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    components: {default: Home, header: Navbar, sidebar: Sidebar, footer: Footer},
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/datakantin',
+    name: 'datakantin',
+    components: {default: Data_Kantin, header: Navbar, sidebar: Sidebar, footer: Footer},
+  },
+  {
+    path: '/datamakanan',
+    name: 'datamakanan',
+    components: {default: Data_Makanan, header: Navbar, sidebar: Sidebar, footer: Footer},
+  },
+  {
+    path: '/pembelian',
+    name: 'pembelian',
+    components: {default: Pembelian  , header: Navbar, sidebar: Sidebar, footer: Footer},
+  },
+
 ]
 
 const router = new VueRouter({
-  routes
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: routes
 })
 
 export default router
